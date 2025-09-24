e00dfd9689bc723b00f58e13838e5ed2fc4eac47
IanMacKenzie <26mackenziei57@stu.smuhsd.org>
2025-09-23 20:48:59 -0700

    I added a new thingy can you do the grid now make sure to use the card i made it'll look good trust me

--- Lab1/Lab1App.swift
+++ Lab1/Lab1App.swift
@@ -6,3 +6,14 @@
  PARTNERS: IAN MACKENZIE, ALEKS LUBZCYNSKIU
  SEPTEMBER 23, 2025
  */
+
+import SwiftUI
+
+@main
+struct Lab1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
