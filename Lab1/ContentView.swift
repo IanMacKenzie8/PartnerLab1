bfb10a589d22cf98c161401e6a366249eae81188
IanMacKenzie <26mackenziei57@stu.smuhsd.org>
2025-09-23 19:28:59 -0700

    #1

--- Lab1/ContentView.swift
+++ Lab1/ContentView.swift
@@ -6,3 +6,16 @@
  PARTNERS: IAN MACKENZIE, ALEKS LUBZCYNSKIU
  SEPTEMBER 23, 2025
  */
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    // Aleks insert the image array here yo
+    
+    // and do the descriptions here
+    
+    // make the dictionary
+    
+    // i'll do the state thingy and the columns after
+}
