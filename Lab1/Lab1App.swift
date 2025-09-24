a6f90f3c7319fbe94903f1b97e3adefdaed7dc89
IanMacKenzie <26mackenziei57@stu.smuhsd.org>
2025-09-23 18:55:58 -0700

    Initial Commit

---
+++ Lab1/Lab1App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab1App.swift
+//  Lab1
+//
+//  Created by cisstudent on 9/23/25.
+//
+
+import SwiftUI
+
+@main
+struct Lab1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
