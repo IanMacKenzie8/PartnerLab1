de2abb24ddaf4e38dbefefa31c1cff0ad5d5e33d
Aleks <lubczal@icloud.com>
2025-09-23 19:44:15 -0700

    Ok I finished the arrays

--- Lab1/ContentView.swift
+++ Lab1/ContentView.swift
@@ -3,19 +3,46 @@
  
  GROUP #1
  
- PARTNERS: IAN MACKENZIE, ALEKS LUBZCYNSKIU
+ PARTNERS: IAN MACKENZIE, ALEKS LUBCZYNSKI
  SEPTEMBER 23, 2025
  */
 
-import SwiftUI
 
+import SwiftUI
 struct ContentView: View {
     
-    // Aleks insert the image array here yo
+    // images
+    let dognames: [String] = [
+        "Airedale Terrier",
+        "American Foxhound",
+        "Dutch Shepherd",
+        "Havanese",
+        "Leonberger",
+        "Mudi",
+        "Norwegian Lundehund",
+        "Pharaoh Hound",
+        "Scottish Terrier",
+        "Tosa"
+    ]
     
-    // and do the descriptions here
+    // decriptions of images at same index
+    let dogDescriptions: [String] = [
+        "The Airedale stands among the world's most versatile dog breeds and has distinguished himself as hunter, athlete, and companion.",
+        "American Foxhounds are good-natured, low-maintenance hounds who get on well with kids, dogs, even cats, but come with special considerations for prospective owners.",
+        "The Dutch Shepherd is a lively, athletic, alert and intelligent breed, and has retained its herding instinct for which it was originally developed.",
+        "Havanese, the only dog breed native to Cuba, are vivacious and sociable companions and are especially popular with American city dwellers.",
+        "The Leonberger is a lush-coated giant of German origin. They have a gentle nature and serene patience and they relish the companionship of the whole family.",
+        "The Mudi is an extremely versatile, intelligent, alert, agile, all-purpose Hungarian farm dog. The breed is a loyal protector of property and family members without being overly aggressive.",
+        "From Norway’s rocky island of Vaeroy, the uniquely constructed Norwegian Lundehund is the only dog breed created for the job of puffin hunting. With puffins now a protected species, today’s Lundehund is a friendly, athletic companion.",
+        "The Pharaoh Hound, ancient Blushing Dog of Malta, is an elegant but rugged sprinting hound bred to course small game over punishing terrain. Quick and tenacious on scent, these friendly, affectionate hounds settle down nicely at home.",
+        "A solidly compact dog of vivid personality, the Scottish Terrier is an independent, confident companion of high spirits. Scotties have a dignified, almost-human character.",
+        "The Tosa's temperament is marked by patience, composure, boldness and courage. He is normally a tranquil, quiet, and obedient dog, with a calm but vigilant demeanor."
+    ]
     
-    // make the dictionary
-    
-    // i'll do the state thingy and the columns after
+    // making the dictionary from the arrays
+    var dogDict: [String: String] {
+        Dictionary(uniqueKeysWithValues: zip(dogNames, dogDescriptions))
+    }
 }
+
+    // Alright I did it, can you start on the columns
